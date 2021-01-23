0cfcfdd20f23ae0b6310335fe0786a4458587270
JordanCurnew <68204139+JordanCurnew@users.noreply.github.com>
2021-01-23 12:53:38 -0500

    Frontend Work - Input sliders and graph

---
+++ inputGraph.js
@@ -0,0 +1,41 @@
+function drawChart(w1, w2, w3){
+    var ctx = document.getElementById("myChart");
+    var data = {
+        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+        datasets: [{
+            label: "f(t) = sin(w1t) + sin(w2t) + sin(w3t)",
+            function: function(x) { return Math.sin(w1*x) + Math.sin(w2*x) + Math.sin(w3*x) },
+            borderColor: "rgba(75, 192, 192, 1)",
+            data: [],
+            fill: false
+        }]
+    };
+
+    Chart.pluginService.register({
+        beforeInit: function(chart) {
+            var data = chart.config.data;
+            for (var i = 0; i < data.datasets.length; i++) {
+                for (var j = 0; j < data.labels.length; j++) {
+                    var fct = data.datasets[i].function,
+                        x = data.labels[j],
+                        y = fct(x);
+                    data.datasets[i].data.push(y);
+                }
+            }
+        }
+    });
+
+    var myBarChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
