f74dc6bd5e067e42bc8b1ce3b399fa6db4f9b05d
JordanCurnew <68204139+JordanCurnew@users.noreply.github.com>
2021-01-23 18:43:16 -0500

    Update DFT_Implementation.js

--- DFT_Implementation.js
+++ DFT_Implementation.js
@@ -1,15 +1,16 @@
 function computeDFT(w1, w2, w3){
     let returnArr = [-1, -1, -1];
-    var pmax = 3000;
-    var N = 20000;
+    var pmax = 300;
+    var N = 1000;
     var gp = math.complex(0,0);
     var arrCounter = 0;
-    for(p = 1000; p <= pmax; p++){
+    for(p = 0; p <= pmax; p++){
         gp = math.complex(0,0);
 
         for(k = 0; k <= N; k++){
             // gp += Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)+Math.sin(w3*2*Math.pi*k/N))* (Math.exp(2*Math.pi*math.complex(0,1)*k*p/N));
-            gp += math.multiply((Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N))) , (Math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N)))));
+            // gp += math.multiply((Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N))) , (Math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N)))));
+            gp = math.add(gp, math.multiply((Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N))) , (Math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N))))));
         }
 
         
