91011606b42d668991f53725b0a2f098fb75a516
Benjamin Beggs <18bb6@caslab.queensu.ca>
2021-01-23 20:42:05 -0500

    Style changes

--- dcp_distrib/js/index.js
+++ dcp_distrib/js/index.js
@@ -40,23 +40,21 @@ async function deployJob() {
   setStatus('Performing Discrete Fourier Transform on the described sum of sines.');
 
   computeBtn.disabled = true;
-
   const job = window.dcp.compute.for(0, 101, function(p){
     progress(0);
     var N = 20000;
     var gp = math.complex(0,0);
     gp = math.complex(0,0);
-
     for(k = 0; k <= N; k++){
-    var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
-    +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
-    var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
-    var t1 = math.multiply(m1, m2);
-    gp = math.add(gp, t1);
-    progress(k / N); // Progress factorial
+      var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
+      +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
+      var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
+      var t1 = math.multiply(m1, m2);
+      gp = math.add(gp, t1);
+      progress(k / N); // Progress factorial
     }
     if(math.abs(gp) > 1){
-    return p;
+      return p;
     }
     return false;
   });
