424110a747fa623f6de0ee4fa803621c907b0929
JordanCurnew <68204139+JordanCurnew@users.noreply.github.com>
2021-01-23 22:41:36 -0500

    Integrated DCL functions with frontend

--- DFT_Implementation.js
+++ DFT_Implementation.js
@@ -7,14 +7,16 @@ function computeDFT(w1, w2, w3, p){
     gp = math.complex(0,0);
 
     for(k = 0; k <= N; k++){
-        var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
-        +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
-        var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
-        var t1 = math.multiply(m1, m2);
+        var m1 = (Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)
+        +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N)));
+
+        var m2 = (math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N))))
+
+        var t1 = Math.multiply(m1, m2);
         gp = math.add(gp, t1);
         progress(k / N); // Progress factorial
     }
-    if(math.abs(gp) > 1){
+    if(Math.abs(gp) > 1){
         return p;
     }
     return false;
--- dcp_distrib/js/index.js
+++ dcp_distrib/js/index.js
@@ -29,19 +29,19 @@ function main() {
 }
 
 async function deployJob() {
+  console.log(document.getElementById('inputA').value);
   const computeBtn = document.getElementById('compute-button');
-  const inputA = document.getElementById('inputA');
-  const inputB = document.getElementById('inputB');
-  const inputC = document.getElementById('inputC');
-  const w1 = Number(inputA.value);
-  const w2 = Number(inputB.value);
-  const w3 = Number(inputC.value);
+  const w1 = Number(document.getElementById('inputA').value);
+  const w2 = Number(document.getElementById('inputB').value);
+  const w3 = Number(document.getElementById('inputC').value);
   setStatus('Performing Discrete Fourier Transform on the described sum of sines.');
-
-  computeBtn.disabled = true;
-  const job = window.dcp.compute.for(1, 100, function(p,w1,w2,w3){
+//w1=10
+//w2=12
+//w3=14
+  computeBtn.disabled = true;       //0 to 100
+  const job = window.dcp.compute.for(9, 15, function(p,w1,w2,w3){
     progress(0);
-    var N = 500;
+    var N = 250;//500
     var gp_re = 0;
     var gp_im = 0;
     for(k = 0; k <= N; k++){
@@ -80,6 +80,7 @@ async function deployJob() {
   job.on('result', logResult);
   job.on('complete', () => {
     computeBtn.disabled = false;
+    drawOutputChart(results[0], results[1], results[2]);
     console.log(results);
   });
 
@@ -107,3 +108,52 @@ function logResult(event) {
 }
 
 document.addEventListener('DOMContentLoaded', main);
+
+
+function drawOutputChart(w1, w2, w3){
+  var ctx = document.getElementById("outputChart");
+  var data = {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+      datasets: [{
+          label: "Transformed DFT function",
+          function: function(x) { 
+              if(x == w1 || x == w2 || x == w3){
+                  return 1
+              }    
+              
+              return 0 
+          },
+          borderColor: "rgba(247,0,0,1.00)",
+          data: [],
+          fill: false
+      }]
+  };
+
+  Chart.pluginService.register({
+      beforeInit: function(chart) {
+          var data = chart.config.data;
+          for (var i = 0; i < data.datasets.length; i++) {
+              for (var j = 0; j < data.labels.length; j++) {
+                  var fct = data.datasets[i].function,
+                      x = data.labels[j],
+                      y = fct(x);
+                  data.datasets[i].data.push(y);
+              }
+          }
+      }
+  });
+
+  var myBarChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+}
