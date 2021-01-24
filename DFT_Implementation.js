f33ba785263eaa044d60033b5ec734192379bf49
Benjamin Beggs <18bb6@caslab.queensu.ca>
2021-01-23 19:05:47 -0500

    Update DFT_Implementation.js

--- DFT_Implementation.js
+++ DFT_Implementation.js
@@ -1,20 +1,20 @@
 function computeDFT(w1, w2, w3){
     let returnArr = [-1, -1, -1];
-    var pmax = 300;
-    var N = 1000;
+    var pmax = 100;
+    var N = 20000;
     var gp = math.complex(0,0);
     var arrCounter = 0;
     for(p = 0; p <= pmax; p++){
         gp = math.complex(0,0);
 
         for(k = 0; k <= N; k++){
-            // gp += Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)+Math.sin(w3*2*Math.pi*k/N))* (Math.exp(2*Math.pi*math.complex(0,1)*k*p/N));
-            // gp += math.multiply((Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N))) , (Math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N)))));
-            gp = math.add(gp, math.multiply((Math.sqrt(N)*(Math.sin(w1*2*Math.pi*k/N)  +   Math.sin(w2*2*Math.pi*k/N)  +   Math.sin(w3*2*Math.pi*k/N))) , (Math.exp(math.multiply(math.complex(0,1),(2*Math.pi*k*p/N))))));
+            var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
+            +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
+            var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
+            var t1 = math.multiply(m1, m2);
+            gp = math.add(gp, t1);
         }
-
-        
-
+        console.log(gp);
         if(Math.abs(gp) > 1){
             returnArr[counter] = gp;
             counter++;
