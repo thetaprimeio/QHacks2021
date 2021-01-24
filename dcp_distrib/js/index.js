f534e713129306bacac5c9177b7910331f49fe85
Benjamin Beggs <18bb6@caslab.queensu.ca>
2021-01-23 21:46:33 -0500

    Fixed function of DFT worker computation

--- dcp_distrib/js/index.js
+++ dcp_distrib/js/index.js
@@ -36,28 +36,30 @@ async function deployJob() {
   const w1 = Number(inputA.value);
   const w2 = Number(inputB.value);
   const w3 = Number(inputC.value);
-
   setStatus('Performing Discrete Fourier Transform on the described sum of sines.');
 
   computeBtn.disabled = true;
-  const job = window.dcp.compute.for(0, 101, function(p){
+  const job = window.dcp.compute.for(8, 16, function(p,w1,w2,w3){
     progress(0);
-    var N = 20000;
-    var gp = math.complex(0,0);
-    gp = math.complex(0,0);
+    var N = 1_000;
+    var gp_re = 0;
+    var gp_im = 0;
     for(k = 0; k <= N; k++){
-      var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
-      +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
-      var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
-      var t1 = math.multiply(m1, m2);
-      gp = math.add(gp, t1);
+      var m1 = Math.sqrt(N)*(Math.sin(w1*2*Math.PI*k/N) +
+      Math.sin(w2*2*Math.PI*k/N) + Math.sin(w3*2*Math.PI*k/N));
+      var m2_re = Math.cos(2*Math.PI*k*p/N);
+      var m2_im = Math.sin(2*Math.PI*k*p/N);
+      var t1_re = m1*m2_re;
+      var t1_im = m1*m2_im;
+      gp_re = gp_re + t1_re;
+      gp_im = gp_im + t1_im;
       progress(k / N); // Progress factorial
     }
-    if(math.abs(gp) > 1){
+    if(Math.sqrt(Math.pow(gp_re, 2) + Math.pow(gp_im, 2)) > 1){
       return p;
     }
     return false;
-  });
+  }, [w1, w2, w3]);
 
   job.public.name = 'Discrete Fourier Transform';
   job.work.on('uncaughtException', (e) => alert(e));
@@ -78,6 +80,7 @@ async function deployJob() {
   job.on('result', logResult);
   job.on('complete', () => {
     computeBtn.disabled = false;
+    console.log(results);
   });
 
   try {
@@ -94,11 +97,11 @@ function setStatus(newStatus) {
 }
 
 function logResult(event) {
-  if (!result.event){
+  console.log(event.result);
+  if (!event.result){
     return;
   }
   results.push(event.result);
-
   //let resultsText;
   //document.getElementById('results').innerText = resultsText;
 }
