b6ca709a3dd0635a74429e9dc1e8865a01ea7716
Benjamin Beggs <18bb6@caslab.queensu.ca>
2021-01-23 20:31:17 -0500

    New changes

--- dcp_distrib/js/index.js
+++ dcp_distrib/js/index.js
@@ -28,42 +28,38 @@ function main() {
     .addEventListener('click', deployJob);
 }
 
-function computeDFT(w1, w2, w3, p){
-  progress(0);
-  var pmax = 100;
-  var N = 20000;
-  var gp = math.complex(0,0);
-  var arrCounter = 0;
-  gp = math.complex(0,0);
-
-  for(k = 0; k <= N; k++){
-      var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
-      +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
-      var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
-      var t1 = math.multiply(m1, m2);
-      gp = math.add(gp, t1);
-      progress(k / N); // Progress factorial
-  }
-  if(math.abs(gp) > 1){
-      return p;
-  }
-  return false;
-}
-
 async function deployJob() {
   const computeBtn = document.getElementById('compute-button');
   const inputA = document.getElementById('inputA');
   const inputB = document.getElementById('inputB');
   const inputC = document.getElementById('inputC');
-  const a = Number(inputA.value);
-  const b = Number(inputB.value);
-  const c = Number(inputC.value);
+  const w1 = Number(inputA.value);
+  const w2 = Number(inputB.value);
+  const w3 = Number(inputC.value);
 
   setStatus('Performing Discrete Fourier Transform on the described sum of sines.');
 
   computeBtn.disabled = true;
 
-  const job = window.dcp.compute.for(0, 101, computeDFT(a,b,c,p));
+  const job = window.dcp.compute.for(0, 101, function(p){
+    progress(0);
+    var N = 20000;
+    var gp = math.complex(0,0);
+    gp = math.complex(0,0);
+
+    for(k = 0; k <= N; k++){
+    var m1 = (math.sqrt(N)*(math.sin(w1*2*math.pi*k/N)
+    +   math.sin(w2*2*math.pi*k/N)  +   math.sin(w3*2*math.pi*k/N)));
+    var m2 = (math.exp(math.multiply(math.complex(0,1),(2*math.pi*k*p/N))))
+    var t1 = math.multiply(m1, m2);
+    gp = math.add(gp, t1);
+    progress(k / N); // Progress factorial
+    }
+    if(math.abs(gp) > 1){
+    return p;
+    }
+    return false;
+  });
 
   job.public.name = 'Discrete Fourier Transform';
   job.work.on('uncaughtException', (e) => alert(e));
